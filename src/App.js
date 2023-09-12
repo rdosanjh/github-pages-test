bff8c5c5b16999f51165a38d6bf03c2350b417c0
Raj Dosanjh <rajdosanjh@rajs-mbp.lan>
2023-09-12 19:34:53 +0100

    setup gh-pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react";
 
 function App() {
+  const [textValue, setTextValue] = React.useState("");
+  const [todos, setTodos] = React.useState([]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" onChange={(e) => setTextValue(e.target.value)} />
+      <button onClick={() => {setTodos([...todos, textValue])}}>Save</button>
+      {todos.map(todo => <p>{todo}</p>)}
     </div>
   );
 }
